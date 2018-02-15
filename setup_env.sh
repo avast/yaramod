c33f2718c21095c8310c928b5b7afd2740d26887
Marek Milkovič <marek.milkovic@avast.com>
2018-02-15 12:27:04 +0100

    Improved setup_env.sh script to detect version of Python and look for different names of binaries

--- setup_env.sh
+++ setup_env.sh
@@ -4,7 +4,27 @@ SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 VIRTUALENV_DIR="${SCRIPT_DIR}/env"
 
 if [ -z ${PYTHON_EXECUTABLE} ]; then
-	PYTHON_EXECUTABLE=python3
+	if [ $(command -v python3) ]; then
+		PYTHON_EXECUTABLE=python3
+	elif [ $(command -v python) ]; then
+		PYTHON_VERSION_STR=$(python --version 2>&1 | sed -r 's/^Python //g')
+		IFS='.' read -r -a PYTHON_VERSION <<< ${PYTHON_VERSION_STR}
+		if [ ${PYTHON_VERSION[0]} -lt 3 ]; then
+			echo "Required Python version is at least 3.0 (yours is ${PYTHON_VERSION_STR}). Please install appropriate version of Python or set PYTHON_EXECUTABLE in your environment to your Python interpreter." >&2
+			exit 1
+		fi
+		PYTHON_EXECUTABLE=python
+	else
+		echo "Could not locate Python with any known name (python3, python). Make sure you have Python 3+ installed and set PYTHON_EXECUTABLE in your environment to your Python interpreter." >&2
+		exit 1
+	fi
+else
+	PYTHON_VERSION_STR=$(${PYTHON_EXECUTABLE} --version 2>&1 | sed -r 's/^Python //g')
+	IFS='.' read -r -a PYTHON_VERSION <<< ${PYTHON_VERSION_STR}
+	if [ ${PYTHON_VERSION[0]} -lt 3 ]; then
+		echo "Required Python version is at least 3.0 (yours is ${PYTHON_VERSION_STR}). Please install appropriate version of Python or set PYTHON_EXECUTABLE in your environment to your Python interpreter." >&2
+		exit 1
+	fi
 fi
 
 ${PYTHON_EXECUTABLE} -m venv "${VIRTUALENV_DIR}"
