ee05464615d2220bc14fd9c8017a9fcbc80815df
Marek Milkovič <marek.milkovic@avast.com>
2018-02-17 14:49:49 +0100

    python: Added more tests for builder bindings

--- tests/python/test_builder.py
+++ tests/python/test_builder.py
@@ -164,20 +164,508 @@ rule rule_2 : Tag2 {
 		true
 }''')
 
-    def test_rule_with_condition(self):
-        cond = yaramod.filesize() >= yaramod.int_val(100)
+    def test_rule_with_string_id_condition(self):
+        cond = yaramod.string_ref('$1')
         rule = self.new_rule \
-            .with_name('rule_with_condition') \
+            .with_name('rule_with_string_id_condition') \
             .with_condition(cond.get()) \
             .get()
         yara_file = self.new_file \
             .with_rule(rule) \
             .get()
 
+        self.assertEqual(yara_file.text, '''rule rule_with_string_id_condition {
+	condition:
+		$1
+}''')
 
-        self.assertEqual(yara_file.text, '''rule rule_with_condition {
+    def test_rule_with_string_at_condition(self):
+        cond = yaramod.match_at('$1', yaramod.int_val(100))
+        rule = self.new_rule \
+            .with_name('rule_with_string_id_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_string_id_condition {
+	condition:
+		$1 at 100
+}''')
+
+    def test_rule_with_string_at_condition(self):
+        cond = yaramod.match_at('$1', yaramod.int_val(100))
+        rule = self.new_rule \
+            .with_name('rule_with_string_id_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_string_id_condition {
+	condition:
+		$1 at 100
+}''')
+
+    def test_rule_with_match_in_range_condition(self):
+        cond = yaramod.match_in_range('$1', yaramod.range(yaramod.int_val(100), yaramod.int_val(200)))
+        rule = self.new_rule \
+            .with_name('rule_with_match_in_range_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_match_in_range_condition {
+	condition:
+		$1 in (100 .. 200)
+}''')
+
+    def test_rule_with_match_count_condition(self):
+        cond = yaramod.match_count('$1')
+        rule = self.new_rule \
+            .with_name('rule_with_match_count_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_match_count_condition {
+	condition:
+		#1
+}''')
+
+    def test_rule_with_match_length_condition(self):
+        cond = yaramod.match_length('$1')
+        rule = self.new_rule \
+            .with_name('rule_with_match_length_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_match_length_condition {
+	condition:
+		!1
+}''')
+
+    def test_rule_with_match_length_with_index_condition(self):
+        cond = yaramod.match_length('$1', yaramod.int_val(0))
+        rule = self.new_rule \
+            .with_name('rule_with_match_length_with_index_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_match_length_with_index_condition {
+	condition:
+		!1[0]
+}''')
+
+    def test_rule_with_match_offset_condition(self):
+        cond = yaramod.match_offset('$1')
+        rule = self.new_rule \
+            .with_name('rule_with_match_offset_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_match_offset_condition {
+	condition:
+		@1
+}''')
+
+    def test_rule_with_match_offset_with_index_condition(self):
+        cond = yaramod.match_offset('$1', yaramod.int_val(0))
+        rule = self.new_rule \
+            .with_name('rule_with_match_offset_with_index_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_match_offset_with_index_condition {
+	condition:
+		@1[0]
+}''')
+
+    def test_rule_with_lt_condition(self):
+        cond = yaramod.filesize() < yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_gt_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_gt_condition {
+	condition:
+		filesize < 100
+}''')
+
+    def test_rule_with_le_condition(self):
+        cond = yaramod.filesize() <= yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_ge_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_ge_condition {
+	condition:
+		filesize <= 100
+}''')
+
+    def test_rule_with_gt_condition(self):
+        cond = yaramod.filesize() > yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_gt_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_gt_condition {
+	condition:
+		filesize > 100
+}''')
+
+    def test_rule_with_ge_condition(self):
+        cond = yaramod.filesize() >= yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_ge_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_ge_condition {
 	condition:
 		filesize >= 100
+}''')
+
+    def test_rule_with_eq_condition(self):
+        cond = yaramod.filesize() == yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_eq_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_eq_condition {
+	condition:
+		filesize == 100
+}''')
+
+    def test_rule_with_neq_condition(self):
+        cond = yaramod.filesize() != yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_neq_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_neq_condition {
+	condition:
+		filesize != 100
+}''')
+
+    def test_rule_with_plus_condition(self):
+        cond = yaramod.filesize() + yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_plus_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_plus_condition {
+	condition:
+		filesize + 100
+}''')
+
+    def test_rule_with_minus_condition(self):
+        cond = yaramod.filesize() - yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_minus_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_minus_condition {
+	condition:
+		filesize - 100
+}''')
+
+    def test_rule_with_multiply_condition(self):
+        cond = yaramod.filesize() * yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_multiply_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_multiply_condition {
+	condition:
+		filesize * 100
+}''')
+
+    def test_rule_with_divide_condition(self):
+        cond = yaramod.filesize() / yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_divide_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_divide_condition {
+	condition:
+		filesize \ 100
+}''')
+
+    def test_rule_with_modulo_condition(self):
+        cond = yaramod.filesize() % yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_modulo_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_modulo_condition {
+	condition:
+		filesize % 100
+}''')
+
+    def test_rule_with_xor_condition(self):
+        cond = yaramod.filesize() ^ yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_xor_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_xor_condition {
+	condition:
+		filesize ^ 100
+}''')
+
+    def test_rule_with_bitwise_and_condition(self):
+        cond = yaramod.filesize() & yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_bitwise_and_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_bitwise_and_condition {
+	condition:
+		filesize & 100
+}''')
+
+    def test_rule_with_bitwise_or_condition(self):
+        cond = yaramod.filesize() | yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_bitwise_or_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_bitwise_or_condition {
+	condition:
+		filesize | 100
+}''')
+
+    def test_rule_with_and_condition(self):
+        cond = yaramod.conjunction([yaramod.filesize() > yaramod.int_val(100), yaramod.filesize() < yaramod.int_val(200)])
+        rule = self.new_rule \
+            .with_name('rule_with_and_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_and_condition {
+	condition:
+		filesize > 100 and filesize < 200
+}''')
+
+    def test_rule_with_or_condition(self):
+        cond = yaramod.disjunction([yaramod.filesize() > yaramod.int_val(100), yaramod.filesize() < yaramod.int_val(200)])
+        rule = self.new_rule \
+            .with_name('rule_with_and_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_and_condition {
+	condition:
+		filesize > 100 or filesize < 200
+}''')
+
+    def test_rule_with_unary_minus_condition(self):
+        cond = -yaramod.int_val(10)
+        rule = self.new_rule \
+            .with_name('rule_with_unary_minus_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_unary_minus_condition {
+	condition:
+		-10
+}''')
+
+    def test_rule_with_not_condition(self):
+        cond = yaramod.not_(yaramod.filesize() < yaramod.int_val(100))
+        rule = self.new_rule \
+            .with_name('rule_with_not_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_not_condition {
+	condition:
+		not filesize < 100
+}''')
+
+    def test_rule_with_bitwise_not_condition(self):
+        cond = ~yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_bitwise_not_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_bitwise_not_condition {
+	condition:
+		~100
+}''')
+
+    def test_rule_with_shift_left_condition(self):
+        cond = yaramod.filesize() << yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_shift_left_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_shift_left_condition {
+	condition:
+		filesize << 100
+}''')
+
+    def test_rule_with_shift_right_condition(self):
+        cond = yaramod.filesize() >> yaramod.int_val(100)
+        rule = self.new_rule \
+            .with_name('rule_with_shift_right_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''rule rule_with_shift_right_condition {
+	condition:
+		filesize >> 100
+}''')
+
+    def test_rule_with_function_call_condition(self):
+        cond = yaramod.id('pe').access('is_dll')()
+        rule = self.new_rule \
+            .with_name('rule_with_function_call_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_module('pe') \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''import "pe"
+
+rule rule_with_function_call_condition {
+	condition:
+		pe.is_dll()
+}''')
+
+    def test_rule_with_structure_access_condition(self):
+        cond = yaramod.id('pe').access('linker_version').access('major')
+        rule = self.new_rule \
+            .with_name('rule_with_structure_access_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_module('pe') \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''import "pe"
+
+rule rule_with_structure_access_condition {
+	condition:
+		pe.linker_version.major
+}''')
+
+    def test_rule_with_structure_access_condition(self):
+        cond = yaramod.id('pe').access('linker_version').access('major')
+        rule = self.new_rule \
+            .with_name('rule_with_structure_access_condition') \
+            .with_condition(cond.get()) \
+            .get()
+        yara_file = self.new_file \
+            .with_module('pe') \
+            .with_rule(rule) \
+            .get()
+
+        self.assertEqual(yara_file.text, '''import "pe"
+
+rule rule_with_structure_access_condition {
+	condition:
+		pe.linker_version.major
 }''')
 
     def test_rule_with_complex_condition(self):
