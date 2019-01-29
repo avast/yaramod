74fb00d7054b21f9bf0fe2242dcc30ead2357e49
Marek Milkovič <marek.milkovic@avast.com>
2019-01-29 17:56:53 +0100

    Small corrections in androguard and cuckoo modules

--- src/types/modules/androguard_module.cpp
+++ src/types/modules/androguard_module.cpp
@@ -27,8 +27,8 @@ bool AndroguardModule::initialize()
 	using Type = Expression::Type;
 
 	auto androguardStruct = std::make_shared<StructureSymbol>("androguard");
+
 	auto certificateStruct = std::make_shared<StructureSymbol>("certificate");
-	androguardStruct->addAttribute(certificateStruct);
 	certificateStruct->addAttribute(std::make_shared<FunctionSymbol>("issuer", Type::Int, Type::Regexp));
 	certificateStruct->addAttribute(std::make_shared<FunctionSymbol>("subject", Type::Int, Type::Regexp));
 	certificateStruct->addAttribute(std::make_shared<FunctionSymbol>("sha1", Type::Int, Type::String));
@@ -37,6 +37,7 @@ bool AndroguardModule::initialize()
 	certificateStruct->addAttribute(std::make_shared<FunctionSymbol>("not_before", Type::Int, Type::Regexp));
 	certificateStruct->addAttribute(std::make_shared<FunctionSymbol>("not_before", Type::Int, Type::String));
 	certificateStruct->addAttribute(std::make_shared<FunctionSymbol>("trusted_by", Type::Int, Type::String));
+	androguardStruct->addAttribute(certificateStruct);
 
 	androguardStruct->addAttribute(std::make_shared<ValueSymbol>("min_sdk", Type::Int));
 	androguardStruct->addAttribute(std::make_shared<ValueSymbol>("max_sdk", Type::Int));
@@ -44,24 +45,38 @@ bool AndroguardModule::initialize()
 
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("displayed_version", Type::Int, Type::Regexp));
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("displayed_version", Type::Int, Type::String));
+
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("url", Type::Int, Type::Regexp));
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("url", Type::Int, Type::String));
+
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("app_name", Type::Int, Type::Regexp));
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("app_name", Type::Int, Type::String));
+
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("permission", Type::Int, Type::Regexp));
+
 	androguardStruct->addAttribute(std::make_shared<ValueSymbol>("permissions_number", Type::Int));
+
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("receiver", Type::Int, Type::Regexp));
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("receiver", Type::Int, Type::String));
+
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("activity", Type::Int, Type::Regexp));
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("activity", Type::Int, Type::String));
+
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("main_activity", Type::Int, Type::Regexp));
+
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("service", Type::Int, Type::Regexp));
 	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("service", Type::Int, Type::String));
 
+	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("filter", Type::Int, Type::Regexp));
+	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("filter", Type::Int, Type::String));
+
+	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("package_name", Type::Int, Type::Regexp));
+	androguardStruct->addAttribute(std::make_shared<FunctionSymbol>("package_name", Type::Int, Type::String));
+
 	auto signatureStruct = std::make_shared<StructureSymbol>("signature");
-	androguardStruct->addAttribute(signatureStruct);
 	signatureStruct->addAttribute(std::make_shared<FunctionSymbol>("hits", Type::Int, Type::Regexp));
 	signatureStruct->addAttribute(std::make_shared<FunctionSymbol>("hits", Type::Int, Type::String));
+	androguardStruct->addAttribute(signatureStruct);
 
 	_structure = androguardStruct;
 	return true;
