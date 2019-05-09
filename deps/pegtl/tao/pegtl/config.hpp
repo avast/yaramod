5a9d8f46e022a97951b6febb962c89efcc3ff605
Tadeas Kucera <tadeas.kucera@avast.com>
2019-05-09 16:36:09 +0200

    add pegtl to dependencies

---
+++ deps/pegtl/tao/pegtl/config.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2017-2019 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/PEGTL/
+
+#ifndef TAO_PEGTL_CONFIG_HPP
+#define TAO_PEGTL_CONFIG_HPP
+
+#if !defined( TAO_PEGTL_NAMESPACE )
+#define TAO_PEGTL_NAMESPACE tao::pegtl
+#endif
+
+// Enable some improvements to the readability of
+// demangled type names under some circumstances.
+// #define TAO_PEGTL_PRETTY_DEMANGLE
+
+#endif
