c5df1a127e5162ce19524925bd79936cd7b0a703
Tadeas Kucera <tadeas.kucera@avast.com>
2019-07-17 11:51:39 +0200

    Rule stores now shared_ptr to its TokenStream

--- include/yaramod/types/rule.h
+++ include/yaramod/types/rule.h
@@ -51,7 +51,7 @@ public:
 	/// @name Constructors
 	/// @{
 	Rule() = default;
-	explicit Rule(TokenStream&& tokenStream, std::string&& name, Rule::Modifier mod, std::vector<Meta>&& metas,
+	explicit Rule(std::shared_ptr<TokenStream> tokenStream, std::string&& name, Rule::Modifier mod, std::vector<Meta>&& metas,
 			std::shared_ptr<StringsTrie>&& strings, Expression::Ptr&& condition,
 			std::vector<std::string>&& tags);
 	Rule(Rule&& rule) = default;
@@ -105,7 +105,7 @@ public:
 	/// @}
 
 private:
-	TokenStream _tokenStream; ///< tokenStream containing all the data in this Rule
+	std::shared_ptr<TokenStream> _tokenStream; ///< tokenStream containing all the data in this Rule
 	std::string _name; ///< Name
 	Rule::Modifier _mod; ///< Modifier
 	std::vector<Meta> _metas; ///< Meta information
