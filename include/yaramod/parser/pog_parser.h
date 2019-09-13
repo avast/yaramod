a64ba6abe401bff1591f8e2713fbeba75bf2b248
Tadeas Kucera <tadeas.kucera@avast.com>
2019-09-13 14:37:38 +0200

    Add PogParser class

---
+++ include/yaramod/parser/pog_parser.h
@@ -0,0 +1,17 @@
+/**
+ * @file src/parser/parser_driver.h
+ * @brief Declaration of class ParserDriver.
+ * @copyright (c) 2017 Avast Software, licensed under the MIT license
+ */
+
+#pragma once
+
+#include <pog/pog.h>
+
+namespace yaramod {
+
+class PogParser{
+
+};
+
+} // namespace yaramod
