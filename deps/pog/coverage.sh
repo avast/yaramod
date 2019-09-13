07f184cdb6a35619d59d04cf68963e663085b39e
Tadeas Kucera <tadeas.kucera@avast.com>
2019-09-13 09:04:53 +0200

    remove pog code, because we want to include it as an external project

--- deps/pog/coverage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./build/tests/pog_tests && \
-	lcov --directory . --capture --output-file coverage.info && \
-	lcov --remove coverage.info '/usr/*' '*/tests/*' --output-file coverage.info && \
-	lcov --list coverage.info
