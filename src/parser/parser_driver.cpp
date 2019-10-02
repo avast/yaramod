1749f320586cd69f57a1c65f46343af9b4c270f2
Tadeas Kucera <tadeas.kucera@avast.com>
2019-10-02 10:54:34 +0200

    Fix newlines in hexstrings

--- src/parser/parser_driver.cpp
+++ src/parser/parser_driver.cpp
@@ -282,7 +282,7 @@ void PogParser::defineTokens()
 	_parser.token(R"(.)").states("@hexstr_multiline_comment");
 	_parser.token(R"(/*//)").states("@hexstr_multiline_comment").enter_state("@hexstr");
 	_parser.token(R"({[ \v\r\t]}*)").states("@hexstr", "@hexstr_jump").action([&](std::string_view) -> Value { return {}; });;
-	_parser.token(R"([\n]*)").states("@hexstr", "@hexstr_jump").action([&](std::string_view) -> Value {
+	_parser.token(R"([\n])").states("@hexstr", "@hexstr_jump").action([&](std::string_view) -> Value {
 		_driver.tmp_counter++;
 		if(_driver.tmp_counter > 50)
 			assert(false);
