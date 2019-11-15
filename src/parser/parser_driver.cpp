2d9550f39c855422efb1a9544c0f67c7350eeb22
Tadeas Kucera <tadeas.kucera@avast.com>
2019-11-15 14:13:33 +0100

    Decrease the number of tokens used for escaped sequences

--- src/parser/parser_driver.cpp
+++ src/parser/parser_driver.cpp
@@ -215,10 +215,7 @@ void ParserDriver::defineTokens()
 		_strLiteral += std::string{str}.substr(2);
 		return {};
 	});
-	_parser.token(R"(\\t)").states("$str").action([&](std::string_view str) -> Value { _strLiteral += std::string{str}; return {}; }); //  '\n',  '\t'
-	_parser.token(R"(\\n)").states("$str").action([&](std::string_view str) -> Value { _strLiteral += std::string{str}; return {}; }); //  '\n',  '\t'
-	_parser.token(R"(\\")").states("$str").action([&](std::string_view str) -> Value { _strLiteral += std::string{str}; return {}; }); //  '"'
-	_parser.token("\\\\\\\\").states("$str").action([&](std::string_view str) -> Value { _strLiteral += std::string{str}; return {}; }); //  '\\'
+	_parser.token(R"(\\[tn\"\\])").states("$str").action([&](std::string_view str) -> Value { _strLiteral += std::string{str}; return {}; }); //  '\n',  '\t'
 	_parser.token(R"(\\[^\"tnx\\])").states("$str").action([&](std::string_view str) -> Value {error_handle(currentLocation(), "Syntax error: Unknown escaped sequence '" + std::string{str} + "'"); return {}; });
 	_parser.token(R"(([^\\"])+)").states("$str").action([&](std::string_view str) -> Value { _strLiteral += std::string{str}; return {}; });
 	_parser.token(R"(\")").states("$str").symbol("STRING_LITERAL").description("\"").enter_state("@default").action([&](std::string_view) -> Value { return emplace_back(STRING_LITERAL, _strLiteral); });
