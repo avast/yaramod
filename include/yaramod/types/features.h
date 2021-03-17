db9a46665be21d18feb65c6438d8f2b2ea923725
Tadeáš Kučera <tadeas.kucera@avast.com>
2021-03-17 23:43:55 +0100

    Improve description in features.h file

--- include/yaramod/types/features.h
+++ include/yaramod/types/features.h
@@ -1,6 +1,6 @@
 /**
  * @file src/types/features.h
- * @brief Declaration of enum TokenType.
+ * @brief Declaration of enum Features.
  * @copyright (c) 2017 Avast Software, licensed under the MIT license
  */
 
@@ -13,14 +13,14 @@ namespace yaramod {
  */
 enum Features
 {
-	Basic = 0x01,          // 0001 - such module is always loaded
-	AvastOnly = 0x02,      // 0010 - such module is loaded when Avast specified
-	VirusTotalOnly = 0x04, // 0100 - such module is loaded when VirusTotal specified
-	Deprecated = 0x08,     // 1000 - such module is deprecated
-	Avast = Basic | AvastOnly,           // 0011 - specification which will load all basic and Avast-specific modules
-	VirusTotal = Basic | VirusTotalOnly, // 0101 - specification which will load all basic and VirusTotal-specific modules
-	AllCurrent = Avast | VirusTotal,     // 0111 - specification which will load all currently used modules
-	Everything = AllCurrent | Deprecated // 1111 - specification which will load everything - even old deprecated modules
+	Basic = 0x01,          // 0001 - such object is always consider
+	AvastOnly = 0x02,      // 0010 - such object is consider when Avast specified
+	VirusTotalOnly = 0x04, // 0100 - such object is consider when VirusTotal specified
+	Deprecated = 0x08,     // 1000 - such object is deprecated
+	Avast = Basic | AvastOnly,           // 0011 - specification which will consider all basic and Avast-specific objects
+	VirusTotal = Basic | VirusTotalOnly, // 0101 - specification which will consider all basic and VirusTotal-specific objects
+	AllCurrent = Avast | VirusTotal,     // 0111 - specification which will consider all currently used objects
+	Everything = AllCurrent | Deprecated // 1111 - specification which will consider everything - even old deprecated objects
 };
 
 } //namespace yaramod
