043e8c9a60bdd947e2ad51407699acee4a5e7d66
Tadeáš Kučera <tadeas.kucera@avast.com>
2021-03-17 20:55:08 +0100

    Call json_to_byte_array with correct paths to modules

--- src/json_to_byte_array.py
+++ src/json_to_byte_array.py
@@ -1,30 +1,41 @@
-# import json
+import getopt
 import os
-# import pathlib
-
-PUBLIC_MODULES_DIRECTORY = '/home/ts/dev/yaramod/src/modules/public'
-PUBLIC_MODULES_AS_CPP_DIRECTORY = '/home/ts/dev/yaramod/src/modules'
+import sys
 
 
-print('Welcome json_to_byte_array.py')
+def main(argv):
+    input_directory = ''
+    output_directory = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print('ERROR: Bad arguments for json_to_byte_array.py: -i <input_directory> -o <output_directory>')
+        sys.exit(2)
 
-def main():
-    for file in os.listdir(PUBLIC_MODULES_DIRECTORY):
+    for opt, arg in opts:
+        if opt == '-h':
+            print('json_to_byte_array.py -i <input_directory> -o <output_directory>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            input_directory = arg
+        elif opt in ("-o", "--ofile"):
+            output_directory = arg
+
+    for file in os.listdir(input_directory):
         filename, extension = os.path.splitext(file)
         if extension == ".json":
-            source_path = os.path.join(PUBLIC_MODULES_DIRECTORY, file)
-            output_file = os.path.join(PUBLIC_MODULES_AS_CPP_DIRECTORY, filename + '.cpp')
-            print(source_path)
+            source_path = os.path.join(input_directory, file)
+            output_file = os.path.join(output_directory, filename + '.cpp')
             with open(source_path, 'r') as f:
                 module_content = f.read()
             array = bytearray(module_content.encode('utf-8'))
 
             with open(output_file, 'wb') as f:
-                print('Written to', output_file)
+                print('Created module', output_file, 'from', filename)
                 f.write(array)
         else:
             continue
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
