4775e291466c4f16d4ba92f24ca319b837543a14
Marek Milkovič <marek.milkovic@avast.com>
2021-04-28 00:11:38 +0200

    Fixed issues with older compilers not providing symbols for std::filesystem functions
    Older GCC needs to be linked against stdc++fs and Clang against c++fs.

---
+++ cmake/FindFilesystem.cmake
@@ -0,0 +1,23 @@
+# Finder for C++ filesystem library.
+#
+# Ensures that stdc++fs or c++fs is used when needed
+# by the compiler.
+
+
+if(NOT TARGET Filesystem::Filesystem)
+	add_library(Filesystem::Filesystem INTERFACE IMPORTED)
+
+	# Clang
+	if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+		# Clang older than 9.x needs -lc++fs
+		if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.0)
+			target_link_libraries(Filesystem::Filesystem INTERFACE c++fs)
+		endif()
+	# GCC
+	elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+		# GCC older than 9.x needs -lstdc++fs
+		if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.0)
+			target_link_libraries(Filesystem::Filesystem INTERFACE stdc++fs)
+		endif()
+	endif()
+endif()
