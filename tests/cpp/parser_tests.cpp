ef3a54ad1b997208d7f06de24120ed67555a6165
msm-code <msm@tailcall.net>
2021-11-29 10:19:23 +0100

    Fix escaped character handling in regex (#191)

--- tests/cpp/parser_tests.cpp
+++ tests/cpp/parser_tests.cpp
@@ -1301,6 +1301,37 @@ rule regexp_with_custom_negative_class
 }
 
 TEST_F(ParserTests,
+RegexpWithOptionalEscapedCharsWorks) {
+	prepareInput(
+R"(
+rule regexp_with_optional_escaped_chars
+{
+	strings:
+		$1 = /1\x32?3/
+	condition:
+		$1
+}
+)");
+
+	EXPECT_TRUE(driver.parse(input));
+	ASSERT_EQ(1u, driver.getParsedFile().getRules().size());
+
+	const auto& rule = driver.getParsedFile().getRules()[0];
+	EXPECT_EQ("regexp_with_optional_escaped_chars", rule->getName());
+	EXPECT_EQ(Rule::Modifier::None, rule->getModifier());
+
+	auto strings = rule->getStrings();
+	ASSERT_EQ(1u, strings.size());
+
+	auto regexp = strings[0];
+	EXPECT_TRUE(regexp->isRegexp());
+	EXPECT_EQ("$1", regexp->getIdentifier());
+	EXPECT_EQ(R"(/1\x32?3/)", regexp->getText());
+
+	EXPECT_EQ(input_text, driver.getParsedFile().getTextFormatted());
+}
+
+TEST_F(ParserTests,
 RegexpWithEscapedSquareBracketsInsideClassWorks) {
 	prepareInput(
 R"(
