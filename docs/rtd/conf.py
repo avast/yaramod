bbf9ff07dec0204cf91c51fd9b7fef220b205a95
Marek Milkovič <marek.milkovic@gendigital.com>
2025-01-10 03:32:09 +0100

    Release v4.1.0

--- docs/rtd/conf.py
+++ docs/rtd/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Avast'
 author = 'Avast'
 
 # The full version, including alpha/beta/rc tags
-release = 'v4.0.2'
+release = 'v4.1.0'
 
 
 # -- General configuration ---------------------------------------------------
